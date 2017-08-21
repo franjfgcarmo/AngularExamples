82ae2c97ccdbe5b19e3d94cddf563726adea5866
Wolfram Sokollek <WolframSokollek@gmail.com>
2017-08-21 15:40:38 +0200

    some inprovements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PoisonModule} from './poison/poison.module';
+import { SimControlsComponent } from './sim-controls/sim-controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimControlsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoisonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
